1ad216e74819e3af0daf4bc2d491b765317b277f
ErickARosales <163037774+ErickARosales@users.noreply.github.com>
2026-06-24 13:41:46 -0600

    Ejercicio_7
    Implementa la funcion esPar

--- ejercicio07.js
+++ ejercicio07.js
@@ -2,8 +2,14 @@
 // Declara una función llamada "esPar" que acepte un parámetro "numero" y retorne verdadero si el número es par, de lo contrario falso
 
 //escribe tu función aquí. Recuerda que el nombre de la función debe ser exactamente "esPar"
-
-
+function esPar (numero) {
+    resto = numero%2;
+    if (resto == 1) {
+        return False;
+    else {
+        return True;
+    }
+}
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
     esPar
